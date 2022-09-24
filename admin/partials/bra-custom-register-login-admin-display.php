6cba3a2c4e4ca9fbf75dec6ac56c7bb7b94563f1
delphix <delphix2012@gmail.com>
2022-09-24 14:46:13 +0100

    Add boilerplate files

---
+++ admin/partials/bra-custom-register-login-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       buildrealapp.co.uk
+ * @since      1.0.0
+ *
+ * @package    Bra_Custom_Register_Login
+ * @subpackage Bra_Custom_Register_Login/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ public/partials/bra-custom-register-login-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       buildrealapp.co.uk
+ * @since      1.0.0
+ *
+ * @package    Bra_Custom_Register_Login
+ * @subpackage Bra_Custom_Register_Login/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
